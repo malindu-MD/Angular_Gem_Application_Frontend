ad631dfa57f57b24b6965869dda3a3dcacd1bb70
malindu-MD <it21173790@my.sliit.lk>
2024-01-23 15:23:27 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MainHeaderComponent} from "./core/main-header/main-header.component";
+import {MainFooterComponent} from "./core/main-footer/main-footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MainHeaderComponent,MainFooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {HomePageComponent} from "./pages/home/home-page/home-page.component";
+import {NotFoundPageComponent} from "./core/not-found-page/not-found-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path:'home',component:HomePageComponent},
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+  {path:'**',component:NotFoundPageComponent}
+
+
+
+];
